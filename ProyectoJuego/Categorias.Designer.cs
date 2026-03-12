751c5da2943831388af020cab9b48420e4cf6883
MontMG <256658966+MontMG@users.noreply.github.com>
2026-03-12 14:39:55 -0600

    Modificación boton
    Me equivoque e intentare cambiar de rama

--- ProyectoJuego/Categorias.Designer.cs
+++ ProyectoJuego/Categorias.Designer.cs
@@ -44,19 +44,17 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Ravie", 28.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(403, 138);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(293, 92);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(525, 88);
+            this.label1.Size = new System.Drawing.Size(373, 62);
             this.label1.TabIndex = 0;
             this.label1.Text = "Categorias";
             // 
             // btnRegresar
             // 
-            this.btnRegresar.Location = new System.Drawing.Point(89, 68);
-            this.btnRegresar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRegresar.Location = new System.Drawing.Point(65, 45);
             this.btnRegresar.Name = "btnRegresar";
-            this.btnRegresar.Size = new System.Drawing.Size(109, 39);
+            this.btnRegresar.Size = new System.Drawing.Size(79, 26);
             this.btnRegresar.TabIndex = 1;
             this.btnRegresar.Text = "Regresar";
             this.btnRegresar.UseVisualStyleBackColor = true;
@@ -64,10 +62,9 @@
             // 
             // btnCerrar
             // 
-            this.btnCerrar.Location = new System.Drawing.Point(1119, 93);
-            this.btnCerrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCerrar.Location = new System.Drawing.Point(814, 62);
             this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(103, 34);
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
             this.btnCerrar.TabIndex = 2;
             this.btnCerrar.Text = "Cerrar";
             this.btnCerrar.UseVisualStyleBackColor = true;
@@ -75,79 +72,72 @@
             // 
             // btnAnimales
             // 
-            this.btnAnimales.Location = new System.Drawing.Point(521, 345);
-            this.btnAnimales.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAnimales.Location = new System.Drawing.Point(379, 230);
             this.btnAnimales.Name = "btnAnimales";
-            this.btnAnimales.Size = new System.Drawing.Size(125, 52);
+            this.btnAnimales.Size = new System.Drawing.Size(91, 35);
             this.btnAnimales.TabIndex = 3;
             this.btnAnimales.Text = "Categoria1";
             this.btnAnimales.UseVisualStyleBackColor = true;
             // 
             // btnVideojuegos
             // 
-            this.btnVideojuegos.Location = new System.Drawing.Point(782, 345);
-            this.btnVideojuegos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnVideojuegos.Location = new System.Drawing.Point(569, 230);
             this.btnVideojuegos.Name = "btnVideojuegos";
-            this.btnVideojuegos.Size = new System.Drawing.Size(173, 52);
+            this.btnVideojuegos.Size = new System.Drawing.Size(126, 35);
             this.btnVideojuegos.TabIndex = 4;
             this.btnVideojuegos.Text = "Categoria2";
             this.btnVideojuegos.UseVisualStyleBackColor = true;
             // 
             // btnCategoria3
             // 
-            this.btnCategoria3.Location = new System.Drawing.Point(294, 468);
-            this.btnCategoria3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCategoria3.Location = new System.Drawing.Point(214, 312);
             this.btnCategoria3.Name = "btnCategoria3";
-            this.btnCategoria3.Size = new System.Drawing.Size(124, 54);
+            this.btnCategoria3.Size = new System.Drawing.Size(90, 36);
             this.btnCategoria3.TabIndex = 5;
             this.btnCategoria3.Text = "Categoria3";
             this.btnCategoria3.UseVisualStyleBackColor = true;
             // 
             // btnPaises
             // 
-            this.btnPaises.Location = new System.Drawing.Point(654, 468);
-            this.btnPaises.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPaises.Location = new System.Drawing.Point(476, 312);
             this.btnPaises.Name = "btnPaises";
-            this.btnPaises.Size = new System.Drawing.Size(113, 54);
+            this.btnPaises.Size = new System.Drawing.Size(82, 36);
             this.btnPaises.TabIndex = 6;
             this.btnPaises.Text = "Categoria4";
             this.btnPaises.UseVisualStyleBackColor = true;
             // 
             // btnCategoria5
             // 
-            this.btnCategoria5.Location = new System.Drawing.Point(986, 468);
-            this.btnCategoria5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCategoria5.Location = new System.Drawing.Point(717, 312);
             this.btnCategoria5.Name = "btnCategoria5";
-            this.btnCategoria5.Size = new System.Drawing.Size(118, 54);
+            this.btnCategoria5.Size = new System.Drawing.Size(86, 36);
             this.btnCategoria5.TabIndex = 7;
             this.btnCategoria5.Text = "Categoria5";
             this.btnCategoria5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(521, 642);
-            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Location = new System.Drawing.Point(379, 428);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(103, 34);
+            this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 8;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(782, 642);
-            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button7.Location = new System.Drawing.Point(569, 428);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(103, 34);
+            this.button7.Size = new System.Drawing.Size(75, 23);
             this.button7.TabIndex = 9;
             this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // Categorias
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1378, 848);
+            this.ClientSize = new System.Drawing.Size(1002, 565);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.btnCategoria5);
@@ -159,9 +149,9 @@
             this.Controls.Add(this.btnRegresar);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Categorias";
             this.Text = "Categorias";
+            this.Load += new System.EventHandler(this.Categorias_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Categorias_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Categorias_MouseMove);
             this.ResumeLayout(false);
